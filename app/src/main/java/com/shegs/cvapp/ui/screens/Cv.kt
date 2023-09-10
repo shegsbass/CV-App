58566f24eb53e1f1105a1c93bd8608e740d284ef
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-11 00:30:22 +0100

    Display personal information on CV Screen

--- app/src/main/java/com/shegs/cvapp/ui/screens/Cv.kt
+++ app/src/main/java/com/shegs/cvapp/ui/screens/Cv.kt
@@ -1,2 +1,14 @@
 package com.shegs.cvapp.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.shegs.cvapp.viewmodel.CvViewModel
+
+@Composable
+fun CvScreen(cvViewModel: CvViewModel) {
+
+    Text(text = cvViewModel.fullName)
+    Text(text = cvViewModel.bio)
+    Text(text = cvViewModel.githubHandle)
+    Text(text = cvViewModel.slackUsername)
+}
