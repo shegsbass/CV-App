f8d93f78989f802f53bf8a9b58e9b85d13947159
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-10 22:57:39 +0100

    Initial commit

---
+++ app/src/main/java/com/shegs/cvapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.shegs.cvapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.shegs.cvapp.ui.theme.CVAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CVAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
