770ea0212a231db3264b971c83603466f2b5e930
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-09-11 00:39:06 +0100

    Edit fields for the EditCv Screen

--- app/src/main/java/com/shegs/cvapp/ui/screens/EditCv.kt
+++ app/src/main/java/com/shegs/cvapp/ui/screens/EditCv.kt
@@ -1,2 +1,45 @@
 package com.shegs.cvapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.shegs.cvapp.viewmodel.CvViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditCvScreen(cvViewModel: CvViewModel) {
+
+    Column(
+        modifier = Modifier
+            .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = cvViewModel.fullName,
+            onValueChange = { cvViewModel.fullName = it }
+        )
+
+        TextField(
+            value = cvViewModel.bio,
+            onValueChange = { cvViewModel.bio = it }
+        )
+
+        TextField(
+            value = cvViewModel.githubHandle,
+            onValueChange = { cvViewModel.githubHandle = it }
+        )
+
+        TextField(
+            value = cvViewModel.slackUsername,
+            onValueChange = { cvViewModel.slackUsername = it }
+        )
+    }
+
+}
